1216fbd17a36b5f15b89c9b288768cd4e16bf77d
Thomas Preston <thomasmarkpreston@gmail.com>
2013-09-13 10:33:23 +0100

    added fbi dependency and fixed viewer bug

--- snapcamera/mode_options.py
+++ snapcamera/mode_options.py
@@ -247,22 +247,46 @@ class ViewerModeOption(ModeOption):
     def __init__(self, *args):
         super().__init__(*args)
         # current image index is the number in the image name
-        try:
-            # get the first image index
-            self.current_image_index = image_index(self.images[0])
-        except IndexError:
-            self.current_image_index = 0
+        # try:
+        #     # get the first image index
+        #     self.current_image_index = image_index(self.images[0])
+        # except IndexError:
+        #     self.current_image_index = None
+        self.current_image_index = 0 if len(self.images) > 0 else None
 
     @property
     def images(self):
         return sorted(os.listdir(IMAGE_DIR))
 
+    @property
+    def current_image(self):
+        if self.current_image_index is not None:
+            return self.images[self.current_image_index]
+        else:
+            return None
+
+    def post_picture(self):
+        # view latest image
+        # if len(self.images) > 0:
+        #     self.current_image_index = len(self.images) - 1
+
+        # view first image
+        if len(self.images) == 1:
+            self.current_image_index = 0
+            self.update_display_option_text()
+            self.start_image_viewer()
+
     def update_display_option_text(self):
-        image_number = image_index(
-            self.images[self.current_image_index])
+        if self.current_image is not None:
+            image_number = image_index(self.current_image)
+        else:
+            image_number = None
         super().update_display_option_text(str(image_number))
 
     def enter(self):
+        if len(self.images) > 0:
+            self.current_image_index = len(self.images) - 1
+
         self.kill_image_viewer()
         self.start_image_viewer()
 
@@ -285,7 +309,10 @@ class ViewerModeOption(ModeOption):
         subprocess.call(['sudo killall fbi'], shell=True)
 
     def start_image_viewer(self):
-        image_file = self.images[self.current_image_index]
+        if self.current_image is None:
+            return
+
+        image_file = self.current_image
         command = 'sudo fbi -autodown -T 1 {image}'.format(
             image=IMAGE_DIR + image_file)
         subprocess.call([command], shell=True)
